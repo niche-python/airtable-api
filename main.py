99e832e136db446d2b097af479aa48db4a1cd8e3
nicheweb <norgerojascerulia@gmail.com>
2024-12-21 19:42:57 +0100

    Api already functional

--- main.py
+++ main.py
@@ -1,25 +1,46 @@
-import os
-from pyairtable import Api, exceptions
+from flask import Flask, request
+from flask_cors import CORS
+import requests
+import json
+
 # only  for development
 import configparser
+
 config = configparser.ConfigParser()
 config.read("config.ini")
-os.environ['AIRTABLE_API_KEY'] = config["Airtable"]["API_KEY"]
-os.environ['AIRTABLE_BASE_ID'] = config['Airtable']['BASE_ID']
-os.environ['AIRTABLE_TABLE_ID'] = config['Airtable']['TABLE_ID']
 
-def main():
+app = Flask(__name__)
+# CORS config for development
+# CORS(app, resources={r"/*": {"origins": "http://localhost:5173"}})
+
+# CORS config for production
+CORS(app, resources={r"/*": {"origins": config['Portfolio_app']['URL']}})
+
+def air_get(full_name, email, message):
     try:
-        api = Api(os.environ['AIRTABLE_API_KEY'])
-        table = api.table(os.environ['AIRTABLE_BASE_ID'],
-                          os.environ['AIRTABLE_TABLE_ID'])
-        answer = table.create(
-            {"Full Name": "Bob", "Email": "bobquebola@gmail.com",
-             "Message": "Necesito contratarte ya"})
-    except exceptions.PyAirtableError:
-        print('Communication error')
+        body = {"records": [{"fields": {"Full Name": full_name, "Email": email,
+                                        "Message": message}}]}
+        headers = {"Content-Type": "application/json",
+                   "Authorization": f"Bearer {config['Airtable']['API_KEY']}"}
+        response = requests.post(config['Airtable']['API_ENDPOINT'],
+                                 headers=headers, data=json.dumps(body))
+        response.raise_for_status()
+
+    except requests.exceptions.ConnectionError as con_error:
+        return json.dumps(con_error)
+    except requests.HTTPError as h_error:
+        return json.dumps(h_error)
     else:
-        print(answer)
+        return response.json()
+
+@app.route("/", methods=['POST'])
+def post_inquiry():
+    inquiry_data = request.json
+    full_name = inquiry_data['name']
+    email = inquiry_data['email']
+    message = inquiry_data['message']
+    answer = air_get(full_name, email, message)
+    return answer
 
 if __name__ == '__main__':
-    main()
+    app.run(debug=True)
